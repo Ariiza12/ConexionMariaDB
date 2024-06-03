0e36e256f9c95e3b10a6832d5cf583db6439c312
Usuario <Usuario@DESARROLLO08.local>
2024-06-03 09:07:04 +0200

    Añadida la conexion con la tabla centros

--- src/ppal/PruebaCentros.java
+++ src/ppal/PruebaCentros.java
@@ -7,12 +7,12 @@ import java.sql.Statement;
 
 import conexion.ConexionBD;
 
-public class PruebaConexion {
+public class PruebaCentros {
 
 	public static void main(String[] args) {
-
+		
 		ConexionBD conexion = new ConexionBD();
-	
+		
 		System.out.println("Conectando a la base de datos...");
 		
 		// Paso 1. Obtener la conexión
@@ -29,17 +29,17 @@ public class PruebaConexion {
 			sentencia = con.createStatement();
 			
 			// Paso 3. Ejecutar la sentencia
-			resultado=sentencia.executeQuery("select cod_empleado, nombre, salario from empleados");
+			resultado=sentencia.executeQuery("select cod_centro, direccion, nombre from centros");
 			
-			System.out.println("Cod. Empleado\tNombre\tSalario");
+			System.out.println("Cod. Centro\tDirección\tNombre");
 			
 			// Paso 4.  Recoger el resultado
 			while(resultado.next()) {
-				int codEmpleado = resultado.getInt("cod_empleado");
+				int codCentro = resultado.getInt("cod_centro");
+				String direccion = resultado.getString("direccion");
 				String nombre = resultado.getString("nombre");
-				int salario = resultado.getInt("salario");
 				
-				System.out.println(codEmpleado+"\t"+nombre+"\t"+salario);
+				System.out.println(codCentro+"\t"+direccion+"\t"+nombre);
 			}
 		} catch (SQLException e) {
 			System.out.println("Error al consultar datos"+e.getMessage());
